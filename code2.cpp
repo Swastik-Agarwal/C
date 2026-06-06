27280b0f3b84cf52521102e79721e543897bdb45
Swastik-Agarwal <aswastik1706@gmail.com>
2026-06-06 15:32:39 +0530

    Add Code1 and Code2

---
+++ code2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){ 
+    // char grade = 'A';
+    // int value = grade; // {type conversion (done by compiler [implicit]) converting small datatype to big}
+    // double price = 100.99; 
+
+    // int newprice = (int)price; // {type casting (done by user [explicit]) converting big datatype to small}
+    // cout << value << endl ;
+    // cout << newprice << endl;
+
+    // // to take the input 
+    // int age;
+    // cout << "Enter the age : ";
+    // cin >> age;
+
+    // cout << "Entered age is " << age << endl;
+
+    // //arithmetic operators
+    int a =10;
+    int b = 3;
+    int c =5;
+    int d = a++;
+    int e = ++b;
+    // cout << (a+b) << endl;
+    // cout << (a/b) << endl;
+    // cout << (a/ (double)b) << endl;
+    // // relational operators 
+    // cout << (a > b) << endl; // return 0 and 1 as false and true 
+    // // // logical operators 
+    // cout << !(a > b) << endl; // logical not operator
+    // cout << ((a > b) || (a < c)) << endl; // logical or operator
+    // cout << ((a > b) && (b < c)) << endl; // logical not operator
+    cout << d << endl;
+    cout << a << endl;
+    cout << e << endl;
+    cout << b << endl;
+
+     
+
+    return 0;
+}
