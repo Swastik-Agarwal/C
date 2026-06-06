33f3311dc85286b62c97ecd4c3cb6ee470aa3883
Swastik-Agarwal <aswastik1706@gmail.com>
2026-06-06 18:28:13 +0530

    Code3 updtaed

--- code3.cpp
+++ code3.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
-// conditional statements if,else,else if 
 int main() {
     int marks;
     cout << "Enter marks: ";
     cin >> marks;
-    if(marks>=90){
-        cout << "Grade ='A'\n";
-    }else if (marks<90 && marks>=80){
-        cout << "Grade = 'B'\n";
+    // // conditional statements if,else,else if 
+    // if(marks>=90){
+    //     cout << "Grade ='A'\n";
+    // }else if (marks<90 && marks>=80){
+    //     cout << "Grade = 'B'\n";
 
-    }else{
-        cout << "Grade = 'C'\n";
-
-    }
-
+    // }else{
+    //     cout << "Grade = 'C'\n";
+    // }
+    // // Ternary Statement
+    cout << (marks>=90 ? "Grade A" : "Grade B" ) << endl;
     return 0;
 }
