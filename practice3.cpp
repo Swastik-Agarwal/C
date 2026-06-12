6bf62518e1984770a45a670a7c566c40aaf645fa
Swastik-Agarwal <aswastik1706@gmail.com>
2026-06-12 21:18:16 +0530

    Files updated an added

--- practice3.cpp
+++ practice3.cpp
@@ -6,25 +6,17 @@ int main(){
     cout << "Enter n:- ";
     cin >> n;
     //char ch='A';
-    int space =n-1;
     for(int i=0;i<n;i++){
-        int num=1;
-        for (int k=0;k<space;k++){
+        for (int k=0;k<n-i;k++){
             cout << ' ' << ' ';
         }
         for(int j=0;j<i+1;j++){
-
-            cout << num << ' ' ;
-            num++;
+            cout << j+1 << ' ' ;
         }
-        num--;
-        for(int l=0;l<i;l++){
-            num--;
-            cout << num << ' ' ;
+        for(int j=i;j>0;j--){
+            cout << j << ' ' ;
         }
-        space--;
         cout << "\n";
     }
-
     return 0;
 }
